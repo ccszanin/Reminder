99095e729cd4a547fde870543e17d9e33920fc3c
Carla Cristine <carlacs.cristine@gmail.com>
2023-09-14 16:52:07 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import {Header} from "./components/Header";
+
 function App() {
-
-
-  return (
-    <h1>Hello</h1>
-  )
+ return(
+  <>
+    <Header />;
+  </>
+ ); 
 }
 
-export default App
+export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+import reminderLogo from "../../assets/images/Logo.jpg";
+
+export function Header(){
+  return <Container>
+   <img src={reminderLogo} alt="Logo React Reminder"  />
+   <button>Adicionar Lembrete</button>
+  </Container>;
+}
